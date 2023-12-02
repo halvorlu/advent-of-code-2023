45f4883f0825c5b1a83e0ca5dbf147f9c818be10
Halvor Lund <halvor.lund@statnett.no>
2023-12-02 10:47:37 +0100

    Day 1

---
+++ day01/src/main.rs
@@ -0,0 +1,23 @@
+use std::io::{stdin, self, BufRead};
+
+fn main()  {
+    let lines = stdin().lock().lines();
+    let mut sum: u32 = 0;
+    for lineresult in lines {
+        let mut firstnum: u32 = 10;
+        let mut lastnum: u32 = 10;
+        let line = lineresult.unwrap();
+        for c in line.chars() {
+            if c.is_numeric() {
+                let num = c.to_digit(10).unwrap();
+                if firstnum == 10 {
+                    firstnum = num;
+                }
+                lastnum = num;
+            }
+        }
+        let linenum = firstnum * 10 + lastnum;
+        sum += linenum;
+    }
+    println!("{}", sum);
+}
